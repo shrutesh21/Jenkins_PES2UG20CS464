a8312de010d76e4ba3e07c50d318c87caa34d5d3
shrutesh21 <90523243+shrutesh21@users.noreply.github.com>
2023-02-17 22:13:53 +0530

    Create try.cpp

---
+++ try.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World, my SRN is PES2UG20CS464\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully changed and commit\n";
+  cout << "This has been added";
+  return 0;
+}
